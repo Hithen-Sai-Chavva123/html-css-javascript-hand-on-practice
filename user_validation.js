8c7b9115244b90d2f70b8ee34d16b8eae44c1708
Hithen-Sai-Chavva123 <chithensai@gmail.com>
2025-02-22 19:25:45 +0530

    Add files via upload

---
+++ user_validation.js
@@ -0,0 +1,15 @@
+<script type="javascript">
+  funtion fun1()
+   if(n1.hit=="")
+   {
+    alert("enter the appropreate userid ")
+   }
+   if(n1.sit=="")
+   {
+    alert("enter the valid password" )
+
+   }
+
+
+
+</script>
